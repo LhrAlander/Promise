ecec1d30f73e82c66f4b3a37f8f17f47596ca9d4
Lin Hairui <hrlin@best-inc.com>
2019-08-20 13:53:12 +0800

    complete Promise Resolution Procedure

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-function Promise (excutor) {
+function MyPromise(excutor) {
   const self = this
 
   self.status = 'pending'
   self.data = undefined
-  self.onResolveCallback = []
-  self.onRejectCallback = []
+  self.onResolvedCallbacks = []
+  self.onRejectedCallbacks = []
 
-  function resolve (value) {
+  function resolve(value) {
     if (self.status === 'pending') {
       self.status = 'fulfilled'
       self.data = value
-      self.onResolveCallback.forEach(cb => cb(value))
+      self.onResolvedCallbacks.forEach(cb => cb(value))
     }
   }
 
-  function reject (error) {
+  function reject(error) {
     if (self.status === 'pending') {
       self.status = 'rejected'
-      self.data = value
-      self.onRejectCallback.forEach(cb => cb(value))
+      self.data = error
+      self.onRejectedCallbacks.forEach(cb => cb(error))
     }
   }
 
@@ -26,5 +26,121 @@ function Promise (excutor) {
     excutor(resolve, reject)
   } catch (error) {
     reject(error)
+  }
+}
+
+MyPromise.prototype.then = function promiseThen(onResolve, onReject) {
+  let self = this
+  onResolve = typeof onResolve === 'function' ? onResolve : function defaultResolve(value) { return value }
+  onReject = typeof onReject === 'function' ? onReject : function defaultReject(reason) { throw reason }
+
+  if (self.status === 'pending') {
+    return new MyPromise(function (resolve, reject) {
+      self.onResolvedCallbacks.push(function (value) {
+        try {
+          const x = onResolve(value)
+          if (x instanceof MyPromise) {
+            x.then(resolve, reject)
+          } else {
+            resolve(x)
+          }
+        } catch (error) {
+          reject(error)
+        }
+      })
+
+      self.onRejectedCallbacks.push(function (reason) {
+        try {
+          const x = onReject(reason)
+          if (x instanceof MyPromise) {
+            x.then(resolve, reject)
+          } else {
+            resolve(x)
+          }
+        } catch (error) {
+          reject(error)
+        }
+      })
+    })
+  }
+
+  if (self.status === 'fulfilled') {
+    return new MyPromise(function (resolve, reject) {
+      try {
+        const x = onResolve(self.data)
+        if (x instanceof MyPromise) {
+          x.then(resolve, reject)
+        } else {
+          resolve(x)
+        }
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+
+  if (self.status === 'rejected') {
+    return new MyPromise(function (resolve, reject) {
+      try {
+        const x = onReject(self.data)
+        if (x instanceof MyPromise) {
+          x.then(resolve, reject)
+        } else {
+          resolve(x)
+        }
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+}
+
+MyPromise.prototype.catch = function promiseCatch(onRejected) {
+  return this.then(null, onRejected)
+}
+
+function resolvePromise(promise, x, resolve, reject) {
+  let then
+  let thenCalledOrThrow = false
+
+  if (promise === x) {
+    return reject(new TypeError('Chaining cycle detected for promise!'))
+  }
+
+  if (x instanceof MyPromise) {
+    if (x.status === 'pending') {
+      x.then(value => {
+        resolvePromise(promise, value, resolve, reject)
+      }, err => {
+        reject(err)
+      })
+    } else {
+      x.then(resolve, reject)
+    }
+    return
+  }
+
+  if ((x !== null && typeof x === 'function') || typeof x === 'object') {
+    try {
+      then = x.then
+      if (typeof then === 'function') {
+        then.call(x, value => {
+          if (thenCalledOrThrow) return
+          thenCalledOrThrow = true
+          resolvePromise(promise, value, resolve, reject)
+        }, err => {
+          if (thenCalledOrThrow) return
+          thenCalledOrThrow = true
+          reject(err)
+        })
+      } else {
+        resolve(x)
+      }
+    } catch (error) {
+      reject(error)
+      return
+    }
+  } else {
+    resolve(x)
   }
 }
